2498e2263f360cf9d236946384433ee5df565563
Sean Ratemo <sean.ratemo@gmail.com>
2023-11-21 15:44:59 +0300

    Reducing image sizes

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('signin');
-// });
+Route::get('/', function () {
+    return view('index');
+});
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
