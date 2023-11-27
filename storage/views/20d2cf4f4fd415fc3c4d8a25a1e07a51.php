891e0ecc9c33dac331c4c4385031ae75f129fc7a
Alison-Kipyegon <131254398+Alison-Kipyegon@users.noreply.github.com>
2023-11-27 09:55:26 +0300

    Delete storage/views directory

--- storage/views/20d2cf4f4fd415fc3c4d8a25a1e07a51.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH G:\xampp\htdocs\HumbleAbode-final\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
--- storage/views/c0717c73a476a66978aaa46fa7f82c69.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH G:\xampp\htdocs\HumbleAbode-final\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
