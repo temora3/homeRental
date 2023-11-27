8a2175a3ad8aea45c55c02f79ede946e1bd3e808
sweenymbuvi <sweenymbuvi@gmail.com>
2023-11-27 21:24:57 +0300

    user profile page

--- resources/views/profile/home.blade.php
+++ resources/views/profile/home.blade.php
@@ -35,3 +35,56 @@
         }
         </style>
 <body>
+ <div class="container mt-4">
+        <form method="POST" enctype="multipart/form-data" id="profile_setup_frm" action="{{ route('logout') }}" class="profile-form">
+            <div class="row">
+            <div class="col-md-4 border-right">
+    <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+        @php
+            $defaultProfilePics = ['pic1.jpg', 'pic2.webp', 'pic3.jpg','pic4.png'];
+            $randomProfilePic = $defaultProfilePics[array_rand($defaultProfilePics)];
+        @endphp
+
+        <img class="rounded-circle mt-5 profile-image" height="250" width="250" src="{{ asset('img/profilepic/' . $randomProfilePic) }}">
+    </div>
+</div>
+
+
+                <div class="col-md-8 border-right">
+                    <div class="p-3 py-5">
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            <h4 class="text-right">My Profile</h4>
+                        </div>
+                        <table class="table table-striped">
+                            <tr><th colspan="2">{{ auth()->user()->username }}'s Details:</th></tr>
+                            <tr><th><i class="bi bi-person-circle"></i> Username</th><td>{{ auth()->user()->username }}</td></tr>
+                            <tr><th><i class="bi bi-envelope"></i> Email</th><td>{{ auth()->user()->email }}</td></tr>
+                            <tr><th><i class="bi bi-person-circle"></i> First name</th><td>{{ auth()->user()->firstname }}</td></tr>
+                            <tr><th><i class="bi bi-person-square"></i> Last name</th><td>{{ auth()->user()->lastname }}</td></tr>
+                            
+                        </table>
+                        <div class="d-flex justify-content-between align-items-center mt-4">
+                    <a href="{{ route('profile.edit') }}" class="btn btn-primary profile-button" style="width: 45%;">Edit Profile</a>
+                    <div style="width: 10%;"></div> <!-- Spacer -->
+                    <form action="{{ route('logout')}}" method="post">
+                        @csrf
+                    <button type="submit" class="btn btn-danger profile-button" style="width:45%">Logout</button>
+    </form>
+    
+
+                    
+                </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+    </div>
+    
+
+
+
+    <script src="{{ asset('js/profileupdate.js') }}"></script>
+</body>
+</html>
