7faad09aa2345fb0003396b4326f284998cb78d4
emilykkimani <143832670+emilykkimani@users.noreply.github.com>
2023-11-28 10:58:08 +0300

    Add files via upload

---
+++ resources/views/search/available.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Colrolib Templates">
+    <meta name="author" content="Colrolib">
+    <meta name="keywords" content="Colrolib Templates">
+    <title>AirBnbs</title>
+
+   <!-- Icons font CSS-->
+   <link href="{{ asset('vendor/mdi-font/css/material-design-iconic-font.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset ('vendor/font-awesome-4.7/css/font-awesome.min.css')}}" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="{{ asset ('https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i')}}" rel="stylesheet">
+    <link href="{{ asset('https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i')}}" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="{{ asset('vendor/select2/select2.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{ asset ('vendor/datepicker/daterangepicker.css')}}" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <!-- <link href="{{ asset ('assets/css/main.css')}}" rel="stylesheet" media="all"> -->
+
+    <!-- Airbnb CSS -->
+    <link href="{{ asset ('assets/css/bnb.css')}}" rel="stylesheet" media="all">
+    <img src="{{asset('images/bg-img-01.jpg')}}" alt= "background image">
+</head>
+<body>
+    <div class="header">
+    <h1>Here is a list of available Airbnbs</h1>
+    </div>
+    <div class="contents">
+    <p> 
+                Safari Lodge<br>
+                Coastal Retreat<br>
+                Nairobi Apartment<br> 
+                Kisumu Lakeside Cottage<br>
+                Amboseli View Chalet<br>
+                Mombasa Condo<br>
+                Rift Valley Escape<br>
+                Nakuru Lakeside Villa<br>
+                Thika Tea Plantation House<br>
+                Nanyuki Mountain Cabin<br>
+                Eldoret Countryside Cottage<br>
+                Kakamega Forest Retreat<br>
+                Lamu Island Cottage<br>
+                Nyeri Tea Farm Bungalow<br>
+                Voi Safari Camp<br>
+                Kericho Tea Estate House<br>
+                Naivasha Lakeside Retreat<br>
+                Kitale Farmhouse<br>
+                Malindi Beach House<br>
+    </p>
+    </div>
+</body>
+</html>
